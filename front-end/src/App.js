d7c2a2ba382cb465aa7184284bcf55fcb7fb5eba
andrecunha10 <andre.cunha10@gmail.com>
2022-03-29 22:21:55 -0300

    Implemented menu with Bootstrap

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,32 @@
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import Logo from './assets/img/toro_ideias_menu.svg'
+
+function Header() {
+  return (
+    <header>
+      <Navbar className='nav-header' expand="sm">
+        <Container>
+          <Navbar.Brand href='/'>
+            <img src={Logo}/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='/' className='text-white'>HOME</Nav.Link>
+              <Nav.Link href='/colaboracao' className='text-white'>COLABORAÇÃO</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
 function App() {
   return (
-    <p>Toró de Ideias</p>
+    <>
+    <Header />
+    </>
   );
 }
 
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/css/style.css'
 
 ReactDOM.render(
   <React.StrictMode>
