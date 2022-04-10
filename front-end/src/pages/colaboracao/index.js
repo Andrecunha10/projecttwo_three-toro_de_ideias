647449e979a01f0e9b95d731db42c1fd29ed78c9
andrecunha10 <andre.cunha10@gmail.com>
2022-04-10 11:09:05 -0300

    Finalize API for collaboration page and fetch page API through fetch

--- front-end/src/pages/colaboracao/index.js
+++ front-end/src/pages/colaboracao/index.js
@@ -1,43 +1,36 @@
-import { Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { Alert, Container } from "react-bootstrap";
 import { MainLayout } from "../../components/layout";
+import { Loading } from "../../components/loading";
 import { PostItCol } from "../../components/Post_it";
 
-const problems = [
-    {
-        id: 1,
-        name: 'Hora Extra',
-        shortDescription: 'Elevado custo com horas extras',
-        color: 'bg-color-green',
-        rotate: 'rotate-5'
-    },
-    {
-        id: 2,
-        name: 'Reclamação Clientes',
-        shortDescription: 'Aumento do número de reclamações de clientes',
-        color: 'bg-color-yellow',
-        rotate: 'rotate5'
-    },
-    {
-        id: 3,
-        name: 'Atrasos na Chegada',
-        shortDescription: 'Funcinários chegam atrasados no posto de trabalho',
-        color: 'bg-color-pink',
-        rotate: 'rotate-5'
-    },
-    {
-        id: 4,
-        name: 'Produto X',
-        shortDescription: 'O que podemos melhorar no Produto X?',
-        color: 'bg-color-blue',
-        rotate: 'rotate5'
-    },   
-]
 
 export function PageColaboracao() {
+    const [problems, setProblems] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [errorMsg, setErrorMsg] = useState()
+    useEffect(() => {
+        fetch('http://localhost:3001/collaborations')
+            .then((response) => response.json())
+            .then((data) => { 
+                setProblems(data)
+            })
+            .catch(() => {
+                setErrorMsg('Falha ao carregar a página. Tente novamente em instantes.')
+            })
+            .finally(() => setLoading(false))
+    }, [])
+    
     return(
         <MainLayout>
             <Container>
                 <h1 className='font-pm fw-bold mt-4 text-center'>Colaboração</h1>
+                {loading && (
+                    <Loading />
+                )}
+                {errorMsg && (
+                    <Alert variant="info" className="text-center my-4">{errorMsg}</Alert>
+                )}
                 <div className='display-grid gap-4 gap-md-5 justify-content-center mt-5'>
                     {problems.map(problem =>(
                         <PostItCol key={problem.id} problem={problem}/>
