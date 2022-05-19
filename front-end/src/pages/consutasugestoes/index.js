f7432a0ef156347a5463a70a02ea0210d7a6b49f
andrecunha10 <andre.cunha10@gmail.com>
2022-05-18 22:56:13 -0300

    correct error in fetchsuggestions

--- front-end/src/pages/consutasugestoes/index.js
+++ front-end/src/pages/consutasugestoes/index.js
@@ -1,5 +1,5 @@
 import { Container } from "react-bootstrap"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { toast } from "react-toastify"
 import { Loading } from "../../components/loading"
 import { getSuggestionUser } from "../../services/suggestions.service"
@@ -12,7 +12,7 @@ export function SuggestionUserPage() {
     const user = useSelector(selectUser)
     const [suggestions, setSuggestions] = useState()
 
-    const fetchSuggestions = async () => {
+    const fetchSuggestions = useCallback( async () => {
         try {
             const data = await getSuggestionUser()
             const filterData = data.filter(item => item.userId === user.id)
@@ -20,11 +20,11 @@ export function SuggestionUserPage() {
         } catch {
             toast.error('Falha ao buscar sugestoes. Tente novamente em instantes.')
         }
-    }
+    },[user.id])
 
     useEffect(() => {
         fetchSuggestions()
-    }, [])
+    }, [fetchSuggestions])
 
     return (
         <LayoutDashboard>
